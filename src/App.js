0cde681dbd6148b8ef54bb8a73be172a4c978b49
payamTaherirostami <payamtaherirostami@gmail.com>
2023-04-28 12:48:31 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import './App.css';
+import { CSVLink } from "react-csv";
+
+
+const csvData = [
+  ["firstname", "Po#", "time"],
+];
+
+const App = () => {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimerOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+const m1 = ("0" + Math.floor((time / 60000) % 60)).slice(-2)
+const m2= ("0" + Math.floor((time / 1000) % 60)).slice(-2)
+const m3 = ("0" + ((time / 10) % 100)).slice(-2)
+
+const myArr= []
+
+const myTime =(new Date(time).toISOString().slice(11, 19))
+function reset () {
+ 
+  alert(`your time is: ${myTime}`)
+  const display= document.querySelector("#In3")
+  display.setAttribute('src',myTime)
+  // inja bayad az form etelaat ro begire
+  myArr.push('payam','99',myTime.toString())
+  csvData.push(myArr)
+  console.log(csvData)
+  setTime(0)
+}
+  return (
+    <div className="Timers">
+      <h2>Start timing your tasks</h2>
+      <div id="display">
+        
+        <span>{m1}:</span>
+        <span>{m2}:</span>
+        <span>{m3}</span>
+
+      </div>
+    
+      <div id="buttons">
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+        {!timerOn && time > 0 && (
+          <button onClick={reset}>Reset</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+      </div>
+      <div>
+      <span>{myTime}</span>
+      <p></p>
+      <CSVLink data={csvData}>Download results</CSVLink>
+
+      {/* <CSVDownload data={csvData} target="_blank" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
